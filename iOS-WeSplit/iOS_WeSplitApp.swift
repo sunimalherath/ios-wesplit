cd6da7255404f9bbc4d783b59523b0689ff79678
Sunimal Herath <sunimal@live.com>
2024-03-23 19:33:14 +1100

    Initial Commit

---
+++ iOS-WeSplit/iOS_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_WeSplitApp.swift
+//  iOS-WeSplit
+//
+//  Created by Sunimal Herath on 23/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
