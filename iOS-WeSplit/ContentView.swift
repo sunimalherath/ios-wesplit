1d99e507d3e77f6b3fbe6c1635d2ae9c03ecce76
Sunimal Herath <sunimal@live.com>
2024-03-24 14:03:37 +1100

    adding things related to forms for reference

--- iOS-WeSplit/ContentView.swift
+++ iOS-WeSplit/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State var name = ""
+    
+    var students = ["John", "Jane", "Michelle"]
+    @State private var selectedStudent = "Michelle"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Enter your name", text: $name)
+                    Text("Your name is \(name)")
+                }
+                
+                Section {
+                    Text("Hello There")
+                    Button("Tap Count: \(tapCount)") {
+                        tapCount+=1
+                    }
+                }
+                
+                Section {
+                    Picker("Select your student", selection: $selectedStudent) {
+                        ForEach(students, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    ForEach(0..<5) { number in
+                        Text("Row \(number)")
+                    }
+                }
+                
+                Section {
+                    ForEach(0..<5) {
+                        Text("Row \($0)")
+                    }
+                }
+            }
+            .navigationTitle("Swift UI")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
